36529203250e2cf9d1b9cbebfce465d8c8ffa8c7
Amalia Hay <amaliawhay@gmail.com>
2020-11-03 18:34:14 -0800

    src updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,22 @@
-/* eslint-disable react/prefer-stateless-function */
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
+import Search from "./pages/search/index";
+import Fav from "./pages/fav/index";
+import NavBar from "./components/navBar/navBar";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+function App() {
+  return (
+    <Router>
+      <div>
+        <NavBar />
+        <Route exact path="/" component={Search} />
+        <Route exact path="/fav" component={Fav} />
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
 export default App;
